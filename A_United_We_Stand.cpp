4ec80dbd8ce8ac4533a17a4ba2f2c2acd57e1424
callrajveer <rehanshugupta1234@gmail.com>
2026-02-18 12:00:15 +0530

    800

--- A_United_We_Stand.cpp
+++ A_United_We_Stand.cpp
@@ -4,8 +4,26 @@ int main(){
     int t;cin>>t;
     while(t--){
         int n;cin>>n;
-        int a[n];
-        for(int i=0; i<n; i++) cin>>a[i];
-        
+        vector<int> a(n);
+        bool iseven=false,isodd=false,diff=false;
+        int prev=-1;
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+        }
+        int maxi=*max_element(a.begin(),a.end());
+        vector<int> b,c;
+        for(int i=0; i<n; i++){
+            if(a[i]!=maxi) b.push_back(a[i]);
+            else c.push_back(a[i]);
+            
+        }
+        if(b.size()==0) cout<<-1<<endl;
+        else{
+            cout<<b.size()<<" "<<c.size()<<endl;
+            for(int i: b) cout<<i<<" ";
+            cout<<endl;
+            for(int i: c) cout<<i<<" ";
+            cout<<endl;
+        }
     }
 }
