2e90e440d07e091f6c0264fd35aec7641ab68f26
callrajveer <rehanshugupta1234@gmail.com>
2026-02-22 06:39:28 +0530

    800

---
+++ A_Twin_Permutations.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;cin>>n;
+        vector<int> a(n);
+        for(int i=0; i<n; i++) cin>>a[i];
+        for(int i=0; i<n; i++){
+            cout<<n+1-a[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
