b9d2098bfff2999904a6c609eb2b7c3ee781682c
callrajveer <rehanshugupta1234@gmail.com>
2026-03-16 16:52:44 +0530

    800

---
+++ A_Jellyfish_and_Undertale.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+      int a,b,n;
+      cin>>a>>b>>n;
+      vector<int> arr(n);
+      for(int i=0; i<n; i++) cin>>arr[i];
+      
+    }
+    return 0;
+}
+
