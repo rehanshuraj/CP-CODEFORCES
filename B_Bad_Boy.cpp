6998f1de5df7d118b5d740d4a93c2196d3c0d89a
callrajveer <rehanshugupta1234@gmail.com>
2026-03-19 22:18:23 +0530

    900

---
+++ B_Bad_Boy.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n,m,x,y;cin>>n>>m>>x>>y;
+        cout<<1<<" "<<1<<" "<< n <<" "<<m<<endl;
+    }
+}
