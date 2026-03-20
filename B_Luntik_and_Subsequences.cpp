9ef8823f9316cf1b8081ff787967599ec2a08df0
callrajveer <rehanshugupta1234@gmail.com>
2026-03-20 12:12:00 +0530

    900

--- B_Luntik_and_Subsequences.cpp
+++ B_Luntik_and_Subsequences.cpp
@@ -3,14 +3,15 @@ using namespace std;
 int main(){
     int t;cin>>t;
     while(t--){
-        int n;cin>>n;
-        vector<int> a(n);
+        long long n;cin>>n;
+        vector<long long> a(n);
         long long cnt0=0,cnt1=0;
-        for(int i=0; i<n; i++){
+        for(long long i=0; i<n; i++){
             cin>>a[i];
             if(a[i]==0) cnt0++;
             if(a[i]==1) cnt1++;
         }
-        cout<<pow(2,cnt0)*cnt1<<endl;
+        long long ans=pow(2,cnt0)*cnt1;
+        cout<<ans<<endl;
     }
 }
