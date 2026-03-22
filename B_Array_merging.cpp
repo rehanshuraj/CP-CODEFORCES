0c9251a874d9b027f766d7842758e5585ce8a237
callrajveer <rehanshugupta1234@gmail.com>
2026-03-22 15:16:09 +0530

    1000

--- B_Array_merging.cpp
+++ B_Array_merging.cpp
@@ -1,51 +1,42 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin>>n;
-        vector <int> a(n),b(n);
-        for (int i=0; i<n; i++) cin>>a[i];
-        for (int i=0; i<n; i++) cin>>b[i];
-        unordered_map <int,int> freq1, freq2;
-        int pre = a[0];
-        int count = 1;
-        for (int i=1; i<n; i++) {
-            if (pre==a[i]) count++;
-            else {
-                if (freq1.count(pre)) {
-                    freq1[pre] = max(freq1[pre], count);
-                }
-                else freq1[pre]=count;
-                pre = a[i];
-                count = 1;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        vector<int> a(n),b(n);
+        for(int i=0; i<n; i++) cin>>a[i];
+        for(int i=0; i<n; i++) cin>>b[i];
+        
+        unordered_map<int,int> mapa,mapb;
+        for(int i=0; i<n;){
+            int j=i;
+            while(j<n && a[i]==a[j]){
+                j++;             
             }
+            mapa[a[i]]=max(mapa[a[i]],j-i);
+            i=j;
         }
-        pre = b[0];
-        count = 1;
-        for (int i=1; i<n; i++) {
-            if (pre==b[i]) count++;
-            else {
-                if (freq2.count(pre)) {
-                    freq2[pre] = max(freq2[pre], count);
-                }
-                else freq2[pre]=count;
-                pre = b[i];
-                count = 1;
+        for(int i=0; i<n; ){
+            int j=i;
+            while(j<n && b[i]==b[j]){
+                j++;             
             }
+            mapb[b[i]]=max(mapb[b[i]],j-i);
+            i=j;
         }
-        int ans = 0;
-        for (auto p : freq1) {
-            if (freq2.count(p.first)) ans = max(ans, p.second+freq2[p.first]);
-            else ans = max(ans, p.second);
+        int ans=1;
+        for(auto& it: mapa){
+            int common=it.first;
+            int value=mapa[common]+mapb[common];
+            ans=max(ans,value);
+        }
+        for(auto& it: mapb){
+            int common=it.first;
+            int value=mapa[common]+mapb[common];
+            ans=max(ans,value);
         }
         cout<<ans<<endl;
+        
     }
-    return 0;
 }
